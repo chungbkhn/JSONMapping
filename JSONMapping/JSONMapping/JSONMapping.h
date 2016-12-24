bd76209f44014b9f1fe9134646f6279fe48247d3
Chung Duong Van <chungbkhn@live.com>
2016-12-24 23:25:04 +0700

    - Init Project

---
+++ JSONMapping/JSONMapping/JSONMapping.h
@@ -0,0 +1,19 @@
+//
+//  JSONMapping.h
+//  JSONMapping
+//
+//  Created by David on 12/24/16.
+//  Copyright © 2016 25ths. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONMapping.
+FOUNDATION_EXPORT double JSONMappingVersionNumber;
+
+//! Project version string for JSONMapping.
+FOUNDATION_EXPORT const unsigned char JSONMappingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONMapping/PublicHeader.h>
+
+
